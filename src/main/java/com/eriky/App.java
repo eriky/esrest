494ca40dbb5f50219d7df6d0cf3b5aaa20ab3142
erik <you@example.com>
2014-11-18 16:54:07 +0100

    added main code

---
+++ src/main/java/com/eriky/App.java
@@ -0,0 +1,13 @@
+package com.eriky;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
